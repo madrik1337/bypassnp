0c9fa8a590ae3540ceca1006186a3c70d68f3274
madrik1337 <116110543+madrik1337@users.noreply.github.com>
2024-12-22 20:38:11 +0300

    Add files via upload

---
+++ robot-bypass-np.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name        Robot-bypass for NotPixel
+// @author       madrik1337
+// @match        https://web.telegram.org/*/*
+// @match        https://notpx.app/*
+// @grant       none
+// @version     1.0
+// @description Убирает табличку с роботом на забаненых аккаунтах в NP.
+// @downloadURL  https://github.com/madrik1337/bypassnp/raw/main/robot-bypass-np.js
+// @updateURL    https://github.com/madrik1337/bypassnp/raw/main/robot-bypass-np.js
+// @homepage     https://github.com/madrik1337/bypassnp
+// ==/UserScript==
+(function () {
+    'use strict';
+
+    // удаление робота
+    function removeRobotSection() {
+        const robotSection = document.querySelector('div._layout_1i63b_1');
+        if (robotSection) {
+            robotSection.remove();
+            observer.disconnect();
+            showNotification();
+        }
+    }
+
+    // уведомление
+    function showNotification() {
+        const notification = document.createElement('div');
+        notification.innerHTML = "&#10003; Bypass Robot activated!";
+        notification.style.position = 'fixed';
+        notification.style.top = '20px';
+        notification.style.left = '50%';
+        notification.style.transform = 'translateX(-50%)';
+        notification.style.backgroundColor = 'black';
+        notification.style.color = 'white';
+        notification.style.padding = '10px 20px';
+        notification.style.borderRadius = '5px';
+        notification.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+        notification.style.fontSize = '16px';
+        notification.style.fontWeight = 'bold';
+        notification.style.zIndex = '9999';
+        document.body.appendChild(notification);
+
+        // кд на увед
+        setTimeout(() => {
+            notification.remove();
+        }, 3000);
+    }
+
+    removeRobotSection();
+
+    const observer = new MutationObserver(() => {
+        removeRobotSection();
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
